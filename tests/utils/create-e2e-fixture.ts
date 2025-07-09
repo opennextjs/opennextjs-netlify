2a10af565caa459869a290de0f6a81410b045079
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-07-09 10:53:05 +0200

    test: make sure all ways to build turbopack fixture would set --turbopack cli toggle

--- tests/utils/create-e2e-fixture.ts
+++ tests/utils/create-e2e-fixture.ts
@@ -316,7 +316,10 @@ async function cleanup(dest: string, deployId?: string): Promise<void> {
 
 export const fixtureFactories = {
   simple: () => createE2EFixture('simple'),
-  helloWorldTurbopack: () => createE2EFixture('hello-world-turbopack'),
+  helloWorldTurbopack: () =>
+    createE2EFixture('hello-world-turbopack', {
+      buildCommand: 'next build --turbopack',
+    }),
   outputExport: () => createE2EFixture('output-export'),
   ouputExportPublishOut: () =>
     createE2EFixture('output-export', {
