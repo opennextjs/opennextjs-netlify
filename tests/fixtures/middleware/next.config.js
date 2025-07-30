22036403820694f1068bf9f476dd413d674422cd
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-07-30 15:36:28 +0200

    test: skip force chunking in middleware fixture

--- tests/fixtures/middleware/next.config.js
+++ tests/fixtures/middleware/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   webpack: (config) => {
     // this is a trigger to generate multiple `.next/server/middleware-[hash].js` files instead of
     // single `.next/server/middleware.js` file
-    config.optimization.splitChunks.maxSize = 100_000
+    // config.optimization.splitChunks.maxSize = 100_000
 
     return config
   },
