a6f1ac36110eda96f7633c9a41deddffc18484ba
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-08-18 10:17:14 +0200

    chore: remove dev/debug logs

--- edge-runtime/lib/cjs.ts
+++ edge-runtime/lib/cjs.ts
@@ -15,12 +15,10 @@ const require = createRequire(import.meta.url)
 let hookedIn = false
 
 function seedCJSModuleCacheAndReturnTarget(matchedModule: RegisteredModule, parent: Module) {
-  console.error('matched', matchedModule.filename)
   if (matchedModule.loaded) {
     return matchedModule.filename
   }
   const { source, filename } = matchedModule
-  console.error('evaluating module', { filename })
 
   const mod = new Module(filename)
   mod.parent = parent
@@ -39,14 +37,12 @@ function seedCJSModuleCacheAndReturnTarget(matchedModule: RegisteredModule, pare
   compiled(mod.exports, createRequire(pathToFileURL(filename)), mod, filename, dirname(filename))
   mod.loaded = matchedModule.loaded = true
 
-  console.error('evaluated module', { filename })
   return filename
 }
 
 const exts = ['.js', '.cjs', '.json']
 
 function tryWithExtensions(filename: string) {
-  // console.error('trying to match', filename)
   let matchedModule = registeredModules.get(filename)
   if (!matchedModule) {
     for (const ext of exts) {
@@ -64,7 +60,6 @@ function tryWithExtensions(filename: string) {
 }
 
 function tryMatchingWithIndex(target: string) {
-  console.error('trying to match', target)
   let matchedModule = tryWithExtensions(target)
   if (!matchedModule) {
     // require("./test") might resolve to ./test/index.js
@@ -84,20 +79,11 @@ export function registerCJSModules(baseUrl: URL, modules: Map<string, string>) {
     registeredModules.set(target, { source, loaded: false, filename: target })
   }
 
-  console.error([...registeredModules.values()].map((m) => m.filename))
-
   if (!hookedIn) {
-    // magic
     // @ts-expect-error - private untyped API
     const original_resolveFilename = Module._resolveFilename.bind(Module)
     // @ts-expect-error - private untyped API
     Module._resolveFilename = (...args) => {
-      console.error(
-        'resolving file name for specifier',
-        args[0] ?? '--missing specifier--',
-        'from',
-        args[1]?.filename ?? 'unknown',
-      )
       let target = args[0]
       let isRelative = args?.[0].startsWith('.')
 
@@ -111,10 +97,8 @@ export function registerCJSModules(baseUrl: URL, modules: Map<string, string>) {
       let matchedModule = tryMatchingWithIndex(target)
 
       if (!isRelative && !target.startsWith('/')) {
-        console.log('not relative, checking node_modules', args[0])
         for (const nodeModulePaths of args[1].paths) {
           const potentialPath = join(nodeModulePaths, target)
-          console.log('checking potential path', potentialPath)
           matchedModule = tryMatchingWithIndex(potentialPath)
           if (matchedModule) {
             break
@@ -123,7 +107,6 @@ export function registerCJSModules(baseUrl: URL, modules: Map<string, string>) {
       }
 
       if (matchedModule) {
-        console.log('matched module', matchedModule.filename)
         return seedCJSModuleCacheAndReturnTarget(matchedModule, args[1])
       }
 
