31f83f540313a4d4eb901d7de36ed5b17711dd51
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-08-20 20:19:11 +0200

    test: comment out test checking expected deploy failure with node middleware

--- tests/integration/edge-handler.test.ts
+++ tests/integration/edge-handler.test.ts
@@ -662,22 +662,22 @@ describe('page router', () => {
   })
 })
 
-test.skipIf(!nextVersionSatisfies('>=15.2.0'))<FixtureTestContext>(
-  'should throw an Not Supported error when node middleware is used',
-  async (ctx) => {
-    await createFixture('middleware-node', ctx)
+// test.skipIf(!nextVersionSatisfies('>=15.2.0'))<FixtureTestContext>(
+//   'should throw an Not Supported error when node middleware is used',
+//   async (ctx) => {
+//     await createFixture('middleware-node', ctx)
 
-    const runPluginPromise = runPlugin(ctx)
+//     const runPluginPromise = runPlugin(ctx)
 
-    await expect(runPluginPromise).rejects.toThrow('Node.js middleware is not yet supported.')
-    await expect(runPluginPromise).rejects.toThrow(
-      'Future @netlify/plugin-nextjs release will support node middleware with following limitations:',
-    )
-    await expect(runPluginPromise).rejects.toThrow(
-      ' - usage of C++ Addons (https://nodejs.org/api/addons.html) not supported (for example `bcrypt` npm module will not be supported, but `bcryptjs` will be supported)',
-    )
-    await expect(runPluginPromise).rejects.toThrow(
-      ' - usage of Filesystem (https://nodejs.org/api/fs.html) not supported',
-    )
-  },
-)
+//     await expect(runPluginPromise).rejects.toThrow('Node.js middleware is not yet supported.')
+//     await expect(runPluginPromise).rejects.toThrow(
+//       'Future @netlify/plugin-nextjs release will support node middleware with following limitations:',
+//     )
+//     await expect(runPluginPromise).rejects.toThrow(
+//       ' - usage of C++ Addons (https://nodejs.org/api/addons.html) not supported (for example `bcrypt` npm module will not be supported, but `bcryptjs` will be supported)',
+//     )
+//     await expect(runPluginPromise).rejects.toThrow(
+//       ' - usage of Filesystem (https://nodejs.org/api/fs.html) not supported',
+//     )
+//   },
+// )
