7492aacf5c94abb76746b1f37f546300892780c5
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-08-21 10:59:02 +0200

    test: add x-runtime res header to middleware-conditions

--- tests/fixtures/middleware-conditions/middleware-shared.ts
+++ tests/fixtures/middleware-conditions/middleware-shared.ts
@@ -4,6 +4,8 @@ import { NextResponse } from 'next/server'
 export function middleware(request: NextRequest) {
   const response: NextResponse = NextResponse.next()
 
+  // report Next.js Middleware Runtime (not the execution runtime, but target runtime)
+  response.headers.append('x-runtime', typeof EdgeRuntime !== 'undefined' ? EdgeRuntime : 'node')
   response.headers.set('x-hello-from-middleware-res', 'hello')
 
   return response
