9c302c0962f0f3b060a500039fd24fa840db28c9
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-08-21 16:45:04 +0200

    chore: remove debug logs

--- tests/fixtures/middleware-i18n/middleware-shared.ts
+++ tests/fixtures/middleware-i18n/middleware-shared.ts
@@ -6,15 +6,12 @@ export async function middleware(request: NextRequest) {
 
   if (response) {
     response.headers.append('Deno' in globalThis ? 'x-deno' : 'x-node', Date.now().toString())
-    console.error('[middleware] adding x-runtime')
     // report Next.js Middleware Runtime (not the execution runtime, but target runtime)
     // @ts-expect-error EdgeRuntime global not declared
     response.headers.append('x-runtime', typeof EdgeRuntime !== 'undefined' ? EdgeRuntime : 'node')
     response.headers.set('x-hello-from-middleware-res', 'hello')
 
     return response
-  } else {
-    console.error('[middleware] NOT adding x-runtime')
   }
 }
 
