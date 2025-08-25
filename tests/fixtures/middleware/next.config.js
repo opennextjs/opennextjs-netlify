ecb849aec84582fef05943a4d453e02336bdf031
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-08-25 20:24:29 +0200

    test: restore chunk splitting in middleware for Edge Runtime case

--- tests/fixtures/middleware/next.config.js
+++ tests/fixtures/middleware/next.config.js
@@ -11,8 +11,11 @@ const nextConfig = {
   webpack: (config) => {
     // this is a trigger to generate multiple `.next/server/middleware-[hash].js` files instead of
     // single `.next/server/middleware.js` file
-    // this doesn't seem to actually work with Node Middleware - it result in next build failures
-    // config.optimization.splitChunks.maxSize = 100_000
+    if (process.env.SPLIT_CHUNKS) {
+      // this doesn't seem to actually work with Node Middleware - it result in next build failures
+      // so we only do this for default/Edge Runtime
+      config.optimization.splitChunks.maxSize = 100_000
+    }
 
     return config
   },
