2341079996db14b980740625bf976ba9dbea2ad6
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-09-24 18:17:15 +0200

    preserve html extension for static files

--- src/adapter/static-assets.ts
+++ src/adapter/static-assets.ts
@@ -1,5 +1,5 @@
 import { cp } from 'node:fs/promises'
-import { join } from 'node:path/posix'
+import { extname, join } from 'node:path/posix'
 
 import type { FrameworksAPIConfig, OnBuildCompleteContext } from './types.js'
 
@@ -11,11 +11,19 @@ export async function onBuildComplete(
 
   for (const staticFile of ctx.outputs.staticFiles) {
     try {
-      await cp(staticFile.filePath, join('./.netlify/static', staticFile.pathname), {
+      let distPathname = staticFile.pathname
+      if (extname(distPathname) === '' && extname(staticFile.filePath) === '.html') {
+        // if pathname is extension-less, but source file has an .html extension, preserve it
+        distPathname += '.html'
+      }
+
+      await cp(staticFile.filePath, join('./.netlify/static', distPathname), {
         recursive: true,
       })
     } catch (error) {
-      throw new Error(`Failed copying static assets`, { cause: error })
+      throw new Error(`Failed copying static asset.\n\n${JSON.stringify(staticFile, null, 2)}`, {
+        cause: error,
+      })
     }
   }
 
