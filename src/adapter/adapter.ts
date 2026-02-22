fbfcc75d861f35bce5d4d2f117a25d3f77a3f65a
pieh <misiek.piechowiak@gmail.com>
2026-02-23 00:17:10 +0100

    unset standalone if supported adapter version is used

--- src/adapter/adapter.ts
+++ src/adapter/adapter.ts
@@ -2,6 +2,7 @@ import { writeFile } from 'node:fs/promises'
 import { join, relative } from 'node:path'
 
 import type { NextAdapter } from 'next-with-adapters'
+import { satisfies } from 'semver'
 
 import { NETLIFY_IMAGE_LOADER_FILE } from '../build/image-cdn.js'
 
@@ -11,10 +12,11 @@ import type { SerializedAdapterOutput } from './adapter-output.js'
 const adapter: NextAdapter = {
   name: 'Netlify',
   modifyConfig(config, ctx) {
+    // TODO(adapter) - to unset output here, we need to know for sure that adapter API version
+    // is one that we support. Currently this hook doesn't declare version. So unsetting standalone
+    // here is risky.
     if (ctx?.phase === 'phase-production-build' && config.output !== 'export') {
-      // TODO(adapter)
-      // If not export, make sure to not build standalone output as it will become useless
-      // // @ts-expect-error types don't unsetting output to not use 'standalone'
+      // If not export, make sure to not build standalone output to avoid wasteful work
       // config.output = undefined
     }
 
@@ -28,6 +30,17 @@ const adapter: NextAdapter = {
     return config
   },
   async onBuildComplete(ctx) {
+    // TODO(adapter): this is just a version I am using for now
+    // if adapter API won't change - this can stay as-is, otherwise version will be bumped
+    // to ensure we only support most recent Adapters API version while it's experimental to avoid
+    // having to support multiple versions of the API at the same time.
+    if (!satisfies(ctx.nextVersion, '>=16.2.0-canary.57')) {
+      // if we don't save an adapter manifest and unset the standalone config,
+      // we will continue to use standalone mode.
+
+      // the config changes
+      return
+    }
     // Convert absolute filePaths to relative (from repoRoot) so the serialized
     // output never contains machine-specific paths. At runtime these are
     // resolved against process.cwd().
@@ -55,6 +68,10 @@ const adapter: NextAdapter = {
     }
 
     await writeFile(join(ctx.distDir, ADAPTER_OUTPUT_FILE), JSON.stringify(serialized), 'utf-8')
+
+    // we can skip some any further work for standalone mode.
+    // @ts-expect-error jsdocs say `undefined` is allowed, but typescript types not allow it.
+    ctx.config.output = undefined
   },
 }
 
